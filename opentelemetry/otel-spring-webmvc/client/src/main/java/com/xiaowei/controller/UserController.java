85b4cf10c32aeffd1d9290313b2e3e8e46853ed6
wangxiaowei14227 <wangxiaowei14227@autohome.com.cn>
2022-01-04 11:17:23 +0800

    collector-contrib

--- opentelemetry/otel-spring-webmvc/client/src/main/java/com/xiaowei/controller/UserController.java
+++ opentelemetry/otel-spring-webmvc/client/src/main/java/com/xiaowei/controller/UserController.java
@@ -2,6 +2,8 @@ package com.xiaowei.controller;
 
 import com.xiaowei.model.User;
 import com.xiaowei.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,15 +14,19 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 public class UserController {
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private IUserService userService;
 
     @GetMapping("/user")
     public User getUser(@RequestParam("id") Long id) {
+        logger.info("Get user by id {}", id);
         return userService.getUser(id);
     }
+
     @PostMapping("/user")
     public User saveUser(@RequestParam("name") String name) {
+        logger.info("Save user with name {}", name);
         return userService.saveUser(name);
     }
 }
